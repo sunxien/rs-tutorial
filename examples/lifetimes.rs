0eb12a9e0a5c27461e0b335f77246ef873ace216
xien.sxe <1025488853@qq.com>
2024-10-28 23:12:43 +0800

    add Examples

--- examples/lifetimes.rs
+++ examples/lifetimes.rs
@@ -25,7 +25,7 @@ struct Employee<'a> {
     id: u32,
     name: &'a str,
 }
-
+/// Lifetime HRTB
 impl<'a: 'b, 'b> Employee<'a> {
     fn test_for_lifetime_on_method(&'a self, ann: &'b str) -> &'a str {
         self.name
@@ -44,10 +44,14 @@ impl Foo {
     }
 }
 
+/// <'_> is anonymous lifetime. It indicates unused lifetime.
 /// Rust Doc1: https://course.rs/basic/lifetime.html
 /// Rust Doc2: https://course.rs/advance/lifetime/advance.html
 #[cfg(test)]
 pub mod lifetimes_test_cases {
+    use std::char::from_u32_unchecked;
+    use std::slice::from_raw_parts;
+    use std::str::from_utf8_unchecked;
     use crate::{Employee, Foo, longest1, longest2};
 
     /// Lifetime is only used for reference.
@@ -85,6 +89,59 @@ pub mod lifetimes_test_cases {
         let r = foo.change_immut_and_share();
         r.share();
         println!("[test_on_method_with_change_mutable] r: {:?}", r);
+    }
+
+    ///
+    #[test]
+    pub fn test_lifetime_for_nll_rule() {
+        let mut str = String::from("NLL");
+        let r1 = &str;
+        let r2 = &str;
+        println!("[test_lifetime_for_nll_rule] {} and {}", r1, r2);
+        // Note: new compiler (since 1.31), scope of r1,r2 is end at here
+
+        // Compile error: Cannot borrow immutable local variable `str` as mutable
+        // NLL is from 1.31
+        let r3 = &mut str;
+        println!("[test_lifetime_for_nll_rule] r3: {}", r3);
+    }
+
+    ///
+    #[test]
+    pub fn test_lifetime_for_reborrow() {
+        let mut foo = Foo {};
+        let mut_ref = &mut foo;
+
+        // reborrow
+        let rb_mut_ref = &*mut_ref;
+        mut_ref.share(); // Improve !!!
+        println!("[test_lifetime_for_reborrow] mutable ref: {:?}", mut_ref);
+        println!("[test_lifetime_for_reborrow] reborrow ref: {:?}", rb_mut_ref);
+
+        mut_ref.share();
+        println!("[test_lifetime_for_reborrow] mutable ref: {:?}", mut_ref);
+    }
+
+    ///
+    #[test]
+    fn test_lifetime_for_static_ref() {
+        let addr = get_inner_literal_memory_addr();
+        let str = locate_value_from_memory(addr.0, addr.1);
+        println!("[test_lifetime_for_static_ref] ptr: {:?}, len: {:?}, value: {:?}", addr.0, addr.1, str);
+    }
+    /// `str` is a variable, but `HelloWorld` is a constant
+    /// `str` scope is to the end of scope, but the literal is live until the process exit
+    fn get_inner_literal_memory_addr() -> (usize, usize) {
+        let str: &'static str = "Hello World";
+        let ptr = str.as_ptr() as usize;
+        let len = str.len();
+        (ptr, len)
+    }
+    /// `HelloWorld` literal is hold in memory.
+    fn locate_value_from_memory(ptr: usize, len: usize) -> &'static str {
+        unsafe {
+            from_utf8_unchecked(from_raw_parts((ptr as *const u8), len))
+        }
     }
 }
 
